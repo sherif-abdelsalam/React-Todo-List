5e32c7f68cdcc96f121a6b9d8eb2b5f9a479625a
Sherif Abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-08-27 23:10:52 +0300

    test

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "gp-test-deployhub"
-    key            = "7bf5776c-6e63-469a-ba5c-a2ab7b7e936d/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
--- terraform/modules/vm/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_id" {
-  value = aws_instance.vm.id
-}
-
-output "public_ip" {
-  value = aws_instance.vm.public_ip
-}
