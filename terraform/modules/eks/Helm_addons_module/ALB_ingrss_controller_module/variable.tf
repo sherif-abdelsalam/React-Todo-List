4d1b287d612ac703ae9049a6adc3f70a75826109
Sherif AbdelSalam <sherif.abdelsalam.111@gmail.com>
2026-08-27 23:51:41 +0300

    Add terraform/modules/eks/Helm_addons_module/ALB_ingrss_controller_module/variable.tf via DeployHub

---
+++ terraform/modules/eks/Helm_addons_module/ALB_ingrss_controller_module/variable.tf
@@ -0,0 +1,20 @@
+variable "serviceaccount_name_alb_ingress" {
+  
+  type = string
+}
+
+variable "alb_controller_IRSA_arn" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_id" {
+    type = string
+}
