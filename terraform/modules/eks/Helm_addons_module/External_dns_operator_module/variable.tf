71e6fb811b424a21b310479dd96c1a988ba093b3
Sherif AbdelSalam <sherif.abdelsalam.111@gmail.com>
2026-08-27 23:51:47 +0300

    Add terraform/modules/eks/Helm_addons_module/External_dns_operator_module/variable.tf via DeployHub

---
+++ terraform/modules/eks/Helm_addons_module/External_dns_operator_module/variable.tf
@@ -0,0 +1,15 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "serviceaccount_name_external_dns" {
+  type = string
+}
+
+variable "external_dns_IRSA_arn" {
+  type = string
+}
