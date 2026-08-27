c1db0048ca53d8c852f1abf49bbd3d3ee53cfb2a
Sherif AbdelSalam <sherif.abdelsalam.111@gmail.com>
2026-08-27 21:13:10 +0300

    Add terraform/modules/network/outputs.tf via DeployHub

---
+++ terraform/modules/network/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+
+output "nat_public_ips" {
+  value = module.vpc.nat_public_ips
+}
+
+output "igw_id" {
+  value = module.vpc.igw_id
+}
